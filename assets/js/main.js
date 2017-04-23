dd7aa1ff4a4fbe690393073370c67a27de9fd911
sue herrera <sueherrera30@gmail.com>
2017-04-23 00:56:28 -0500

    experimentando modal con conejitos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,47 @@
+var contenedor = document.getElementById('contenedor');
 
-var contenedor =document.getElementsByClassName('contenedor')[0];
-var rosa = document.getElementById('rosa');
-var azul = document.getElementById('azul');
-var verde = document.getElementById('verde');
-var amarillo = document.getElementById('amarillo');
 
-contenedor.style.position="relative";
-contenedor.style.background ="DarkGrey";
-contenedor.style.width ="600px";
-contenedor.style.padding="50px";
+//Traer el modal
+var modal = document.getElementById('miModal');
+modal.style.background="grey";
+modal.style.display="none";
 
-azul.style.position="absolute";
-azul.style.top= "250px";
-azul.style.right= "0";
+
+
+
+// obtener la imagen e insertarla dentro del modal y se mostrará al hacer click
+
+var imagenConejito = document.getElementById('imagenConejito');
+var imagenModal = document.getElementById('laImagenNueva');
+imagenConejito.style.position="absolute";
+modal.style.display="inlineBlock";
+
+imagenConejito.addEventListener("click",mostrar);
+
+function mostrar(){
+  modal.style.display="block";
+  imagenModal.src=this.src;
+  imagenModal.align="left";
+  modal.style.display= "inlineBlock";
+  modal.style.zIndex = "1";
+  modal.style.background="grey";
+  modal.style.width="1500px"
+  modal.style.height="1000px"
+
+
+  }
+
+//se cerrara la imagen creada atravez del tachecito
+
+var tachecito = document.getElementsByClassName('cerrar')[0];
+tachecito.addEventListener("click",cerrar);
+tachecito.style.color="white";
+tachecito.style.fontSize = "large";
+
+
+
+
+function cerrar(){
+  modal.style.display ="none";
+
+}
